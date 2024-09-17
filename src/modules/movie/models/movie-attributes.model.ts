c3a0bf1fbf6904a148eef4394d0c1b08bc4f3038
Felipe Lacerda <felipelacerda500@gmail.com>
2024-09-17 20:57:20 -0300

    feat: add movie attributes model

---
+++ src/modules/movie/models/movie-attributes.model.ts
@@ -0,0 +1,8 @@
+export type Rating = {
+  valor: number;
+  fonte: string;
+};
+
+export type Synopsis = { texto: string; idioma: string };
+
+export type Award = { nome: string; relevancia: number };
